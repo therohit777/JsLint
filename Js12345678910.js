6ae7dd225350b617df317f500c37da761176327d
Rohit Pandey <rohitpandeyofficial777@gmail.com>
2024-01-06 22:44:03 +0530

    lecture 9 and 10 added

--- Js12345678910.js
+++ Js12345678910.js
@@ -26,15 +26,44 @@
 // Alert statement: Renders stuff through popup
    alert("Rohit is a good boy");
 
+
 // Datatypes
-   String
-   Number
-   Boolean
-   BigInt: Very big Number
-   NULL: standalone value
-   undefined: means value not assigned
-   symbol: uniqnueness of a component
-   NaN: Not a number
+   Based on type of data storage and way to access 
+   it is of 2types:
+
+   1. Primitive:  
+        i.  Call by value(changes done in copy)
+        ii. Stack memory
+         String
+         Number
+         Boolean
+         BigInt: Very big Number
+         NULL: standalone value
+         undefined: means value not assigned
+         symbol: uniqnueness of a component or value
+         NaN: Not a number
+
+      Example: 
+      const id1 = Symbol('123');
+      const id2 = Symbol('123'); //Symbol
+      const bigNumber = 1234567890000n; // add n at end to make it a BigInt number
+      const arr = [1,2,3,4,5]; //array
+      const myObj = { //object
+         name:'Rohit',
+         roll: '1234',
+      }
+      const print = function(){ //Function
+         console.log("Hello world")
+      }
+      console.log(id1===id2); False
+
+
+   2. Non-Primitive/Reference:
+        i.  Call by Reference(changes done in original memory reference)
+        ii. Heap memory
+            Array 
+            Objects
+            Functions      
 
 
 
@@ -44,7 +73,8 @@
    typeof Null => Object
    typeof undefined => undefined
    typeof NaN => Number
-
+   typeof 100.3 => Number
+   typeof myfunc => Object Function.
 
 
 // Conversion of datatypes
@@ -63,7 +93,6 @@
       33 => '33'
 
 
-
 // Operations
     1. Add: +
     2. Sub: -
@@ -78,7 +107,6 @@
     console.log("1" + "2") => 12
     console.log("1" + 2 + 2) => 122 
     console.log(1 + 2 + "2") => 32
-
 
     
 // Comparison
@@ -101,3 +129,26 @@
    1. ===
    2. !== 
    (It checks value and datatypes both unlike ==,!=)
+
+
+// Javascript is Dynamic or Statically typed ?
+    Dynamically typed as datatype of variable are determind
+    by the value it holds in runtime. Typescript is statically 
+    typed.
+
+
+// Memory allocation
+   2 types of memory:
+      1. Stack => 
+         i.  Primitive variables uses it.
+         ii. If assign primitive variable value
+             to others variable here copy of 
+             primitive variables value is made and 
+             assigned to other variable.
+      2. Heap => 
+         i.  Non-Primitive variable uses it.
+         ii. If assign primitive variable value
+             to others variable here memory 
+             reference of primitive variable 
+             original value is made and assigned 
+             to other variable.
