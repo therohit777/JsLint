a49c43b3af6e013674117b69fbb6a4286d1481ab
Rohit Pandey <rohitpandeyofficial777@gmail.com>
2024-01-08 01:19:11 +0530

    operator, maths,number,string, arrays methods

---
+++ Js1112131415.js
@@ -0,0 +1,159 @@
+// // Strings
+//    const name = "Rohit";
+//    const Name = new String("Rohit"); 
+//    make character object of String.
+//    {
+//     0:'R',
+//     1:'o',
+//     2:'h',
+//     3:'i',
+//     4:'t',
+//    }
+
+// // String Interpolation
+//    const names = "Rohit";
+//    console.log(`${names} is a good boy`); 
+
+// // String Methods
+//    const Name = new String("Rohit"); 
+
+//    1. Access character: Name[0]
+//    2. String length:    Name.length 
+//    3. uppercase:        Name.toUpperCase()
+//    4. lowercase:        Name.toLowerCase()
+//    5. character index:  Name.charAt(index)
+//    6. add string:       Name.concat('Pandey');
+//    7. find position of 
+//       cahracter:        Name.indexOf('R')
+//    8. Substring:        Name.substring(startindex, endindex); //end index is not inclusive
+//                         Name.slice(start,end); // Same as substring but we can also use negative values here
+//    9. Trim:             Name.trim() //Removes starting or ending spaces or newline only
+//    10. Replace:          Name.Replace('substring insdie name', 'new string as it replacer')
+//    11.check whether:    Name.includes("hit")
+//       substring is 
+//       present or not
+
+//    12. Conversion of:   Name.split(spilliting charater) 
+//        String into      const address = "I am from india"
+//        array of         console.log(address.split(' ')); => ['I', 'am', 'from', 'india']
+//        characters
+
+
+// // Number
+//    const id1 = 120;
+//    const id2 = new Number(120); =>[Numer: 120] output
+
+// // Number Methods
+//    const num = 10;
+
+//    1. string conversion: num.toString();
+//    2. precision valueL:  num.toFixed(2); => 10.00
+//    3. conversion to:     num.toLocaleString('en-IN')
+//       India format
+
+// // Maths Methods
+//    1. Conersion to:     Math.abs(-10) => 10
+//       positive values
+//    2. Round off:        Math.round(4.6) => 5
+//    3. upper bound:      Math.ceil(4.2) =>5
+//    4. lower bound:      Math.floor(4.2) =>4
+//    5. square root:      Math.sqrt(25) =>5
+//    6. max value:        Math.max(1,2,3,4,5) =>5
+//    6. min value:        Math.min(1,2,3,4,5) =>1
+//    7. random values:    Math.random() // gives random values between 0 to 1
+
+//    //Note: To get random values within a range:
+//      const min = 10;
+//      const max = 30;
+//      consol.log(Math.floor(Math.random()*(max-min+1))+min);
+//      Math.floor( Math.random() * (max - min + 1)) + min => gives random values within a Range.END_TO_END
+
+
+
+
+
+
+// // Date and Time
+//    let myDate = new Date() //declaration
+//    let myDates = new Date('01-12-2023') //declaration
+//    typeof myDate //Object, In js month starts from 0
+
+// // Date and  Time methods
+//    1. Get timestamp:  myDate.getTime() //gives time in millisecond values
+//    2. Get current:    Date.now()
+//       timestamp
+//    3. Get timestamp:  Date.now()/1000
+//       in seconds
+//    4. Get month:      myDate.getMonth() + 1;
+//    5. Get Day:        myDate.getDay()
+
+//    Note:
+//    Date in milliseconds is used to know in 
+//    fastest fingerfast in Kbc or at time of 
+//    booking stays at Airbnb.
+   
+
+
+
+   
+
+// // Arrays
+//    1. collection of items of different datatypes
+//    2. resizable by its own in javascript.
+//    3. does shallow copy 
+//    4. 0 based indexing
+//    5. Accessing: arrayname[indexvalue] 
+//    const arr = [1, 2, 3, 4, 5, true, 'Rohit']; //Declaration
+//    const arr2 = new Array(1, 2, 3, 4, 5); //Declaration
+
+// // Array Methods:
+//    1. Add items at end:   arr.push(10);
+//    2. delete last item:   arr.pop();
+//    3. Add items start:    arr.unshift(10);
+//    4. Del first item:     arr.shift();
+//    5. check items 
+//       present or not:     arr.includes(4); //true
+//    6. Get index:          arr.indexOf(3); //2, -1 if not exists
+//    7. Convert to string:  arr.join(); // "1,2,3,4,5" => String
+//    8. Get subarray:       arr.slice(start,endindex); //here end index is not included
+//    9. Get subarray        arr.splice(start,endindex); //end index is inclusive
+//       and remove its
+//       subarray part from 
+//       original array.
+//    10.merge two arrays:   arr1.concat(arr2);
+//       and return new
+//       array
+
+
+//    Note:
+//      Slice                      vs    Splice
+//      1.Last index exclusive           1.Inclusive
+//      2.Original array is not          2.Original arrays subarray/splice  
+//        modified.                        part is removed.
+//
+//      arr1.push(arr2) => [1,2,3,[4,5,6]] //as array in js can store different datatypes together
+     
+
+// //Spread Operator    
+//    It opens the existing Array (...)
+//    example:
+//    const arr1 = [1,2,3];
+//    const arr2 = [4,5,6];
+//    const arr3 = [...arr1,...arr2]; // => [1,2,3, 4,5,6];
+
+// // Want to remove arrays inside arrays.
+//    const arr4  = [1,2,3,[4,5,6,[7.8,9]],[10]];
+//    arr4.flat(Infinity); //Infinity is the depth of subarrays inside array => [1,2,3,4,5,6,7,8,9]
+
+// // Array check and creation
+//    1. Array check:     Array.isArray(arr1) //true
+//    2. array create:    I Array.of(100,200,300); //[100,200,300]
+//                       II Array.from(10,20);
+                      
+//    Note: Array.of({socre1:100}); // Gives empty array as we have define that we have to create keys a array.
+
+
+    
+
+
+
--- Js12345678910.js
+++ Js12345678910.js
@@ -1,154 +1,154 @@
-// Print Statement
- console.log("Hello world")
+// // Print Statement
+//  console.log("Hello world")
 
-// Execute Node js in local system.
-  1. Uses node V8 engine
-  2. command: nodex filename.js
+// // Execute Node js in local system.
+//   1. Uses node V8 engine
+//   2. command: node filename.js
 
-// Variables in Javascript(const,Let,var)
-   const: value once assigned cannot be changed
-   let: value once assigned can pe changed
-   var: value once assigned can be changed and 
-        redeclared (Prefer not to use because Of
-        issue of of block and functional scope.) 
-   Example:
-   const name = "Rohit";
-   let id = 12310;
-   let city; //value is undefined as nothing is assigned to it.
+// // Variables in Javascript(const,Let,var)
+//    const: value once assigned cannot be changed
+//    let: value once assigned can pe changed
+//    var: value once assigned can be changed and 
+//         redeclared (Prefer not to use because Of
+//         issue of of block and functional scope.) 
+//    Example:
+//    const name = "Rohit";
+//    let id = 12310;
+//    let city; //value is undefined as nothing is assigned to it.
 
-// Comments
-   1. singleline: //
-   2. multiline : /**/
+// // Comments
+//    1. singleline: //
+//    2. multiline : /**/
 
-// To use newer version of JS:
-   "use strict";
+// // To use newer version of JS:
+//    "use strict";
 
-// Alert statement: Renders stuff through popup
-   alert("Rohit is a good boy");
-
-
-// Datatypes
-   Based on type of data storage and way to access 
-   it is of 2types:
-
-   1. Primitive:  
-        i.  Call by value(changes done in copy)
-        ii. Stack memory
-         String
-         Number
-         Boolean
-         BigInt: Very big Number
-         NULL: standalone value
-         undefined: means value not assigned
-         symbol: uniqnueness of a component or value
-         NaN: Not a number
-
-      Example: 
-      const id1 = Symbol('123');
-      const id2 = Symbol('123'); //Symbol
-      const bigNumber = 1234567890000n; // add n at end to make it a BigInt number
-      const arr = [1,2,3,4,5]; //array
-      const myObj = { //object
-         name:'Rohit',
-         roll: '1234',
-      }
-      const print = function(){ //Function
-         console.log("Hello world")
-      }
-      console.log(id1===id2); False
+// // Alert statement: Renders stuff through popup
+//    alert("Rohit is a good boy");
 
 
-   2. Non-Primitive/Reference:
-        i.  Call by Reference(changes done in original memory reference)
-        ii. Heap memory
-            Array 
-            Objects
-            Functions      
+// // Datatypes
+//    Based on type of data storage and way to access 
+//    it is of 2types:
+
+//    1. Primitive:  
+//         i.  Call by value(changes done in copy)
+//         ii. Stack memory
+//          String
+//          Number
+//          Boolean
+//          BigInt: Very big Number
+//          NULL: standalone value
+//          undefined: means value not assigned
+//          symbol: uniqnueness of a component or value
+//          NaN: Not a number
+
+//       Example: 
+//       const id1 = Symbol('123');
+//       const id2 = Symbol('123'); //Symbol
+//       const bigNumber = 1234567890000n; // add n at end to make it a BigInt number
+//       const arr = [1,2,3,4,5]; //array
+//       const myObj = { //object
+//          name:'Rohit',
+//          roll: '1234',
+//       }
+//       const print = function(){ //Function
+//          console.log("Hello world")
+//       }
+//       console.log(id1===id2); False
+
+
+//    2. Non-Primitive/Reference:
+//         i.  Call by Reference(changes done in original memory reference)
+//         ii. Heap memory
+//             Array 
+//             Objects
+//             Functions      
 
 
 
-// To know datatype of variable(typeof).
-   typeof 
-   Note:
-   typeof Null => Object
-   typeof undefined => undefined
-   typeof NaN => Number
-   typeof 100.3 => Number
-   typeof myfunc => Object Function.
+// // To know datatype of variable(typeof).
+//    typeof 
+//    Note:
+//    typeof Null => Object
+//    typeof undefined => undefined
+//    typeof NaN => Number
+//    typeof 100.3 => Number
+//    typeof myfunc => Object Function.
 
 
-// Conversion of datatypes
-  Number conversion: Number()
-      "10" => 10
-      "10abc" => NaN
-      "true" => 1
-      Example:  let a = '10'
-                let ab = Number(a);
-  Boolean conversion:
-      1 => true
-      0 => false
-      "Rohit" => true
-      "" => False
-  String conversion:
-      33 => '33'
+// // Conversion of datatypes
+//   Number conversion: Number()
+//       "10" => 10
+//       "10abc" => NaN
+//       "true" => 1
+//       Example:  let a = '10'
+//                 let ab = Number(a);
+//   Boolean conversion:
+//       1 => true
+//       0 => false
+//       "Rohit" => true
+//       "" => False
+//   String conversion:
+//       33 => '33'
 
 
-// Operations
-    1. Add: +
-    2. Sub: -
-    3. Divide: /
-    4. Multiply: *
-    5. remainder: %
-    6. power: **
-    7. ++a => increments in same statement
-    8. a++ => increments in next statement
+// // Operations
+//     1. Add: +
+//     2. Sub: -
+//     3. Divide: /
+//     4. Multiply: *
+//     5. remainder: %
+//     6. power: **
+//     7. ++a => increments in same statement
+//     8. a++ => increments in next statement
 
-    console.log('Hello' + 'Rohit') => Hello Rohit
-    console.log("1" + "2") => 12
-    console.log("1" + 2 + 2) => 122 
-    console.log(1 + 2 + "2") => 32
+//     console.log('Hello' + 'Rohit') => Hello Rohit
+//     console.log("1" + "2") => 12
+//     console.log("1" + 2 + 2) => 122 
+//     console.log(1 + 2 + "2") => 32
 
     
-// Comparison
-   1. >
-   2. <
-   3. ==
-   4. !=
-   5. >=
-   6. <=
-   console.log(null > 0); false
-   console.log(null == 0); false
-   console.log(null >= 0); true
+// // Comparison
+//    1. >
+//    2. <
+//    3. ==
+//    4. !=
+//    5. >=
+//    6. <=
+//    console.log(null > 0); false
+//    console.log(null == 0); false
+//    console.log(null >= 0); true
 
-   console.log(undefined > 0); false
-   console.log(undefined == 0); false
-   console.log(undefined >= 0); false
-
-
-// strict checking: 
-   1. ===
-   2. !== 
-   (It checks value and datatypes both unlike ==,!=)
+//    console.log(undefined > 0); false
+//    console.log(undefined == 0); false
+//    console.log(undefined >= 0); false
 
 
-// Javascript is Dynamic or Statically typed ?
-    Dynamically typed as datatype of variable are determind
-    by the value it holds in runtime. Typescript is statically 
-    typed.
+// // strict checking: 
+//    1. ===
+//    2. !== 
+//    (It checks value and datatypes both unlike ==,!=)
 
 
-// Memory allocation
-   2 types of memory:
-      1. Stack => 
-         i.  Primitive variables uses it.
-         ii. If assign primitive variable value
-             to others variable here copy of 
-             primitive variables value is made and 
-             assigned to other variable.
-      2. Heap => 
-         i.  Non-Primitive variable uses it.
-         ii. If assign primitive variable value
-             to others variable here memory 
-             reference of primitive variable 
-             original value is made and assigned 
-             to other variable.
+// // Javascript is Dynamic or Statically typed ?
+//     Dynamically typed as datatype of variable are determind
+//     by the value it holds in runtime. Typescript is statically 
+//     typed.
+
+
+// // Memory allocation
+//    2 types of memory:
+//       1. Stack => 
+//          i.  Primitive variables uses it.
+//          ii. If assign primitive variable value
+//              to others variable here copy of 
+//              primitive variables value is made and 
+//              assigned to other variable.
+//       2. Heap => 
+//          i.  Non-Primitive variable uses it.
+//          ii. If assign primitive variable value
+//              to others variable here memory 
+//              reference of primitive variable 
+//              original value is made and assigned 
+//              to other variable.
